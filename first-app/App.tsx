9eb10348fc03c8565f8b1b1736149515927c98d6
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-07-23 16:27:37 +0200

    Added image

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.welcomeTxt}>
-      <Text>Welcome to my app!</Text>
-      <Text>Enter your name:</Text>
+    <View >
+      <Image style={styles.mainImg}source={require('./images/coding.png')}/>
+
+      <Text style={styles.welcomeTxt}>Welcome to my app!</Text>
+      <Text style={styles.welcomeTxt}>Enter your name:</Text>
       <TextInput placeholder="John"/>
-      <Text>Enter your surname:</Text>
+      <Text style={styles.welcomeTxt}>Enter your surname:</Text>
       <TextInput placeholder="Carter"/>
 
       <Button title="Add User"/>
@@ -18,10 +20,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   welcomeTxt: {
-    paddingTop: 40,
+    paddingTop: 50,
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 30,
     textAlign: 'center'
   },
+
+  headingTxt: {
+    fontWeight: 'bold',
+  },
+
+  inputBoxTxt : {
+
+  },
+
+  mainImg: {
+    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 250,
+    width: 250
+  }
+  
 });
