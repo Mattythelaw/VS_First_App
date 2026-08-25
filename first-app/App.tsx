f55c38fdad5cb47f60a2b3555eabd1e49a553e60
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-08-25 16:56:46 +0200

    Added important comments

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -14,8 +14,10 @@ import { RadioButton } from 'react-native-paper'
     };
   };
 
+  // This sets up the "naviagtor" which controls teh switching of screens
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
+  // This describes what information gets passed in each screen
   type MainScreenProps = NativeStackScreenProps<
     RootStackParamList,
     'Home'
@@ -26,11 +28,15 @@ import { RadioButton } from 'react-native-paper'
     'View'
   >;
 
+// This is the first thing that gets displayed when the app opens
 export default function App() {
 
   return (
+    // Everthing that is related to navigation has to stay inside of this
     <NavigationContainer>
       <Stack.Navigator>
+         {/* This tells the app that the "Home" screen is howing the Mainscreen component and the "View" screen
+         is hwoing the ViewDetails component */}
         <Stack.Screen name = "Home" component= {Mainscreen}/>
         <Stack.Screen name = "View" component= {ViewDetails}/>
       </Stack.Navigator>
@@ -38,15 +44,22 @@ export default function App() {
   );
 }
 
+// This is the Home screen and the "naviagtion" is a remote control that allows you to navigate to 
+// to the other screens
 function Mainscreen({ navigation }: 
   MainScreenProps){
 
+  // useState is how the component will remember something
+  // Name is the current value and setName is the way to change it
+  // Note for self: Always use setName and never change Name directly
   const [Name, setName] = useState('');
   const [Surname, setSurname] = useState('');
   const [Error, setError] = useState(false);
 
+  //Sends a message to the terminal to let you let you know if the app works
   console.log('App works!');
-  
+
+  // A normal function that makes the first letter capital
   const capitalize = (text: string) => {
     if (text.length === 0) return text;
     return text.charAt(0).toUpperCase() + text.slice(1);
@@ -56,18 +69,27 @@ function Mainscreen({ navigation }:
   return(
 
     <View >
+      {/* Lets you scroll if needed */}
       <SafeAreaView>
         <ScrollView>
+      {/* "require()"  will grab a picture from my project folder */}    
       <Image style={styles.mainImg}
       source={require('./images/coding.png')}/>
 
       <Text style={styles.welcomeTxt}>Welcome to my app!</Text>
       
     <FadeInView>
-      <Text style={Error? styles.redTxt : styles.blank}>{Error? "Please enter your info": ""}</Text>
+
+      {/* If the error is true it display in red, if not then nothing */}
+      <Text style={Error? styles.redTxt : styles.blank}>
+        {Error? "Please enter your info": ""}
+      </Text>
+
       <View style={styles.inputFlex}>
       <Text style={styles.headingTxt}>Enter your name:</Text>
 
+      {/* This bos shows whatever "Name" is currently in and everytime 
+      a letter is typed it capitalizes and saves it */}
       <TextInput style={styles.inputBoxTxt}placeholder="John"
       value={Name}
       onChangeText={newText => setName(capitalize(newText))} 
@@ -86,8 +108,10 @@ function Mainscreen({ navigation }:
       <Button title="Add User"
         onPress={() => {
 
+        // Can only proceed if both the boxes have something typed in
         if ((isEmpty(Name)==false) && (isEmpty(Surname)==false)){
 
+          // Proceed to the "View" screen and with the name and surname
           navigation.navigate('View', {
             NameSend : Name,
             SurnameSend : Surname
@@ -95,6 +119,7 @@ function Mainscreen({ navigation }:
           setError(false);
         }
         else{
+          // makes the "please fill this in" text show
           setError(true);
         }
         
