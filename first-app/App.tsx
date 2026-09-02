b6841e6c1a8f14acb69c7f52a7c96ca0b30676ff
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-09-02 16:54:01 +0200

    Added list skill page and functionality

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -220,11 +220,56 @@ function ViewDetails({ navigation, route }: ViewDetailsProps){
 }
 
 function ListSkills({ navigation, route }: ListSkillsProps){
+  const [Skills] = useState<string[]>([]);
   const [txtSkill, setSkill] = useState('');
+
+  const renderSkills = () => {
+    const arrOutput = [];
+
+    for (let i=0; i < Skills.length; i++){
+      arrOutput.push(
+        <Text key={i} style={styles.skillText}>
+          {Skills[i]}
+        </Text>
+      )
+    }
+    return arrOutput;
+  }
   
   return(
-    <View>
+    // Entire app container for eveything on the page
+    <View style={styles.appContainer}>
+      <View>
+        {/* Adding ability to scroll down the page */}
+        <SafeAreaView>
+          <ScrollView>
+            {/* assigning BOTH mainImg and bannerImg style to our image */}
+            <View style={styles.mainImg}>
+              <Image style={styles.bannerImg}
+                    source={require('./images/banner.jpg')}/>
+            </View>
+            {/* Our main heading for the image */}
+            <Text style={styles.welcomeTxt}>List your Skills!</Text>
 
+            {/* Where the users types their skills */}
+            <View style={styles.inputContainer}>
+              <TextInput style={styles.textInput}
+                        placeholder='Enter your skills'
+                        onChangeText={newText => setSkill(newText)}
+              />
+              <Button title='Add skill'
+                onPress={() => {
+                  Skills.push(txtSkill);
+                  setSkill("");
+                }}/>
+
+            </View>
+                <View style={styles.skillContainer}>
+                  {renderSkills()}
+                </View>
+          </ScrollView>
+        </SafeAreaView>
+      </View>
     </View>
   )
 
