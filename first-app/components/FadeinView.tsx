f8e70e80ffed40859f3c210ceded734cc75444bc
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-09-09 08:44:00 +0200

    Moved fadeinview code to FadeInView.tsx

--- first-app/components/FadeinView.tsx
+++ first-app/components/FadeinView.tsx
@@ -1,3 +1,30 @@
 import React from 'react';
 import {useRef, useEffect} from 'react'
 import {Animated} from 'react-native'
+
+const FadeInView = ({children, style}: FadeInViewProps) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current
+
+
+useEffect(() => {
+  Animated.timing(
+    fadeAnim,
+    {
+      toValue: 1,
+      duration: 4000,
+      useNativeDriver: false
+    }
+  ).start();
+}, [fadeAnim])
+
+return(
+  <Animated.View style = {{
+    ...(style as object),
+    opacity: fadeAnim
+  }}>
+    {children}
+  </Animated.View>
+  );
+
+}
+export default FadeInView;
